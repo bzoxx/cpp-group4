7977019c41f431317aace52ac5738135af1b0469
Biniyam Abebe <Biniyamabe16@gmail.com>
2024-01-20 20:35:47 +0300

    ddlkjfsda

--- Pattern b.cpp
+++ Pattern b.cpp
@@ -4,45 +4,68 @@ int main(){
 int n,spaces;
 n=9;
 for(int i=0; i!=n; i++){
+if(i==0){
+for(int j=1; j<=n-i; j++){
+    cout<<i;
+}}else{
 for(int j=0; j<=n-i; j++){
     cout<<i;
 }
+}
 if(i==0){
     spaces = 0;
+    cout<<"0";
 }else if(i==1){
     spaces = 1;
-    for(int j=0; j<=spaces; j++){
+    for(int j=0; j!=spaces; j++){
     cout<<" ";
 }
 }else{
 spaces+=2;
-for(int j=0; j<=spaces; j++){
+for(int j=0; j!=spaces; j++){
     cout<<" ";
 }}
+if(i==0){
+for(int j=1; j<=n-i; j++){
+    cout<<i;
+}}else{
 for(int j=0; j<=n-i; j++){
     cout<<i;
+}
 }
     cout<<endl;
 }
 spaces = (2*n)+1;
 for(int i=n; i!=-1; i--){
+if(i==0){
+for(int j=1; j<=n-i; j++){
+    cout<<i;
+}}else{
 for(int j=0; j<=n-i; j++){
     cout<<i;
 }
+}
 if(i==0){
-    spaces = 0;
+    //spaces = 0;
+    cout<<"0";
 }else if(i==1){
     spaces = 1;
-    for(int j=0; j<=spaces; j++){
+    for(int j=0; j!=spaces; j++){
     cout<<" ";}
 }else{
 spaces-=2;
-for(int j=0; j<=spaces; j++){
+
+for(int j=0; j!=spaces; j++){
     cout<<" ";
 }
 }
+if(i==0){
+for(int j=1; j<=n-i; j++){
+    cout<<i;
+}}else{
 for(int j=0; j<=n-i; j++){
     cout<<i;
+}
 }
     cout<<endl;
 }
